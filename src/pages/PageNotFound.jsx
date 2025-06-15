b88dbc848808f3c81d5ced23b5e82542d95f5c60
Mihajlo Gluhovic <mihajlogluhovic@gmail.com>
2025-06-15 21:32:19 +0200

    Reinitializing repo

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <h1>PageNotFound</h1>
+    </div>
+  );
+}
+export default PageNotFound;
