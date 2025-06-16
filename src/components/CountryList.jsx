be50911937325171012cff6f1f5c16c281133504
Mihajlo Gluhovic <mihajlogluhovic@gmail.com>
2025-06-17 01:21:15 +0200

    City, form and params

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,20 +1,26 @@
-import CityItem from "./CityItem";
+import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
 
-function CountryList({ countries, isLoading }) {
+function CountryList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
-  if (!countries.length)
+  if (!cities.length)
     return (
       <Message message="Add your first city buy clicking on a city on the map" />
     );
 
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
   return (
     <ul className={styles.countryList}>
-      {countries.map((city) => (
-        <CityItem city={city} key={city.id} />
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
